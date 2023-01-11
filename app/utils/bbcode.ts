01093d143f23d29b4d455b54ea10b8bc71a5ef94
Spuxx <ameisenfutter@outlook.com>
2023-01-12 00:52:53 +0100

    some basic post content parsing

--- app/utils/bbcode.ts
+++ app/utils/bbcode.ts
@@ -1,6 +1,7 @@
+import { htmlSafe } from '@ember/template';
 import bbCodeParser from 'js-bbcode-parser';
 
 export function parsePostContent(input: string) {
   const output = bbCodeParser.parse(input);
-  return output;
+  return htmlSafe(output);
 }
