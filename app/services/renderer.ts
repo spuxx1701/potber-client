eb401b150d7128700384b166dd1c43c05b6397eb
Spuxx <ameisenfutter@outlook.com>
2023-01-21 10:10:36 +0100

    removed old main-nav

--- app/services/renderer.ts
+++ app/services/renderer.ts
@@ -12,29 +12,7 @@ export default class RendererService extends Service {
   preventScrollReset = false;
 
   @action initialize() {
-    this.updateMainNavPosition();
     this.updateBoxStyle();
-  }
-
-  @action updateMainNavPosition() {
-    const mainNavPosition = this.localStorage.getMainNavPosition();
-    if (mainNavPosition === 'top') {
-      this.rootStyle.setProperty('--main-nav-top', '0px');
-      this.rootStyle.setProperty('--main-nav-bottom', 'unset');
-      this.rootStyle.setProperty(
-        '--page-content-padding-top',
-        'var(--main-nav-height)'
-      );
-      this.rootStyle.setProperty('--page-content-padding-bottom', '0px');
-    } else {
-      this.rootStyle.setProperty('--main-nav-top', 'unset');
-      this.rootStyle.setProperty('--main-nav-bottom', '0px');
-      this.rootStyle.setProperty('--page-content-padding-top', '0px');
-      this.rootStyle.setProperty(
-        '--page-content-padding-bottom',
-        'var(--main-nav-height)'
-      );
-    }
   }
 
   @action updateBoxStyle() {
