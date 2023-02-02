3ddcf8728380835c7bac4ad51930d38d1fa1b32a
Spuxx <ameisenfutter@outlook.com>
2023-02-02 16:21:33 +0100

    enabled basic bb tags in post form

--- app/components/board/post-form/index.ts
+++ app/components/board/post-form/index.ts
@@ -26,12 +26,32 @@ export default class PostFormComponent extends Component<Signature> {
     return this.args.submitLabel || 'Absenden';
   }
 
+  get textarea() {
+    const textarea = document.getElementById('post-form-textarea');
+    if (textarea) {
+      return textarea as HTMLTextAreaElement;
+    } else throw new Error('post-form-textarea could not be found.');
+  }
+
   @action handleTitleChange(value: string) {
     this.args.post.title = value;
   }
 
   @action handleMessageChange(event: InputEvent) {
     this.args.post.message = (event.target as HTMLInputElement).value;
+  }
+
+  @action insertTag(openingTag: string, closingTag: string) {
+    const selectionStart = this.textarea.selectionStart;
+    const selectionEnd = this.textarea.selectionEnd;
+    const message = this.args.post.message;
+    this.args.post.message =
+      message.substring(0, selectionStart) +
+      openingTag +
+      message.substring(selectionStart, selectionEnd) +
+      closingTag +
+      message.substring(selectionEnd, message.length);
+    this.textarea.value = this.args.post.message;
   }
 
   @action handleSubmit(event: SubmitEvent) {
