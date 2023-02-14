2d8d1fede19c2152b0a130f52e43cb1e90d46d3d
Spuxx <ameisenfutter@outlook.com>
2023-02-14 10:53:45 +0100

    posts endpoint is more restful at the cost of a circular depdendency

--- app/controllers/authenticated/post/create.ts
+++ app/controllers/authenticated/post/create.ts
@@ -5,10 +5,14 @@ import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { PostFormContent } from 'potber-client/components/board/post-form';
 import { PostCreateRouteModel } from 'potber-client/routes/authenticated/post/create';
+import CustomStore from 'potber-client/services/custom-store';
+import MessagesService from 'potber-client/services/messages';
 
 export default class PostCreateController extends Controller {
   declare model: PostCreateRouteModel;
 
+  @service declare store: CustomStore;
+  @service declare messages: MessagesService;
   @service declare router: RouterService;
   @tracked busy = false;
 
@@ -16,16 +20,30 @@ export default class PostCreateController extends Controller {
 
   @action async handleSubmit(post: PostFormContent) {
     this.busy = true;
-    // const postId = await this.posts.createPost(this.model.thread.id, post);
-    const postId = 'TODO';
-    this.busy = false;
-    if (postId) {
-      this.router.transitionTo('authenticated.thread', {
-        queryParams: {
-          TID: this.model.thread.id,
-          PID: postId,
-        },
+    try {
+      const newPost = this.store.createRecord('post', {
+        threadId: this.model.thread.id,
+        ...post,
       });
+      await newPost.save();
+      if (newPost.id) {
+        this.router.transitionTo('authenticated.thread', {
+          queryParams: {
+            TID: this.model.thread.id,
+            PID: newPost.id,
+          },
+        });
+      }
+    } catch (error) {
+      this.messages.logErrorAndNotify(
+        'Das hat leider nicht geklappt.',
+        error,
+        this.constructor.name
+      );
     }
+    this.busy = false;
+
+    // const post = await this.store.createRecord('post', {});
+    // const postId = 'TODO';
   }
 }
