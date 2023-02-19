6a6e38974a4c6a0336fa26a526c05af247c5a74b
Spuxx <ameisenfutter@outlook.com>
2023-02-19 12:16:09 +0100

    minor improvements on input control

--- app/components/common/control/input/index.ts
+++ app/components/common/control/input/index.ts
@@ -1,14 +1,15 @@
 import Component from '@glimmer/component';
 import { guidFor } from '@ember/object/internals';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 interface Signature {
   Args: {
     value: string;
-    title?: string;
+    label?: string;
     size?: ControlSize;
-    onChange?: (value: string) => void;
+    required?: boolean;
+    selectAllOnFocus?: boolean;
+    onChange?: (value: string, event: InputEvent) => void;
   };
 }
 
@@ -16,16 +17,29 @@ export default class CommonInputComponent extends Component<Signature> {
   declare args: Signature['Args'];
   componentId = 'input-' + guidFor(this);
 
-  @tracked value = this.args.value;
+  get value() {
+    return this.args.value;
+  }
 
   get size() {
     return this.args.size || 'medium';
   }
 
+  get label() {
+    return this.args.label;
+  }
+
+  @action handleFocus(event: FocusEvent) {
+    if (this.args.selectAllOnFocus) {
+      const input = event.target as HTMLInputElement;
+      input.setSelectionRange(0, input.value.length);
+    }
+  }
+
   @action handleChange(event: InputEvent) {
-    this.value = (event.target as HTMLInputElement).value;
+    const value = (event.currentTarget as HTMLInputElement).value;
     if (this.args.onChange) {
-      this.args.onChange(this.value);
+      this.args.onChange(value, event);
     }
   }
 }
