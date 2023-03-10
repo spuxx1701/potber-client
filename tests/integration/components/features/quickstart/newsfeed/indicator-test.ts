15487e6fd5c76056547e5c95c461d310c51c89ad
Spuxx <ameisenfutter@outlook.com>
2023-03-10 21:23:35 +0100

    fixed test module description

--- tests/integration/components/features/quickstart/newsfeed/indicator-test.ts
+++ tests/integration/components/features/quickstart/newsfeed/indicator-test.ts
@@ -1,4 +1,4 @@
-import { find, render, TestContext } from '@ember/test-helpers';
+import { render, TestContext } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'potber-client/tests/helpers';
 import NewsfeedService from 'potber-client/services/newsfeed';
@@ -14,70 +14,77 @@ interface Context extends TestContext {
   element: HTMLElement;
 }
 
-module('Integration | Component | Newsfeed | Indicator', function (hooks) {
-  setupRenderingTest(hooks);
+module(
+  'Integration | Component | Feature | Quickstart | Newsfeed | Indicator',
+  function (hooks) {
+    setupRenderingTest(hooks);
 
-  test('should be hidden when there are no news', async function (this: Context, assert) {
-    class NewsfeedStub extends NewsfeedService {
-      unreadBookmarks: Bookmark[] = [];
-    }
-    this.owner.register('service:newsfeed', NewsfeedStub);
-    await render(hbs`<Features::Quickstart::Newsfeed::Indicator/>`);
-
-    assert.dom('[data-test-newsfeed-indicator]').hasStyle({ display: 'none' });
-  });
-
-  test('should be hidden when there are news, but sidebar is visible', async function (this: Context, assert) {
-    class NewsfeedStub extends NewsfeedService {
-      unreadBookmarks: Bookmark[] = [newsfeedMocks.unreadBookmark];
-    }
-    this.owner.register('service:newsfeed', NewsfeedStub);
-    class RendererStub extends RendererService {
-      leftSidebarExpanded = true;
-    }
-    this.owner.register('service:renderer', RendererStub);
-    await render(hbs`<Features::Quickstart::Newsfeed::Indicator/>`);
-
-    assert.dom('[data-test-newsfeed-indicator]').hasStyle({ display: 'none' });
-  });
-
-  test("should have class 'newsfeed-indicator-info' when there are unread bookmarks", async function (this: Context, assert) {
-    class NewsfeedStub extends NewsfeedService {
-      unreadBookmarks: Bookmark[] = [newsfeedMocks.unreadBookmark];
-    }
-    this.owner.register('service:newsfeed', NewsfeedStub);
-    await render(hbs`<Features::Quickstart::Newsfeed::Indicator/>`);
-
-    assert
-      .dom('[data-test-newsfeed-indicator')
-      .hasClass('newsfeed-indicator-info');
-  });
-
-  test("should have class 'newsfeed-indicator-position-right' when left-sided sidebar layout is selected", async function (this: Context, assert) {
-    class SettingsStub extends SettingsService {
-      get sidebarLayout() {
-        return SidebarLayout.leftTop;
+    test('should be hidden when there are no news', async function (this: Context, assert) {
+      class NewsfeedStub extends NewsfeedService {
+        unreadBookmarks: Bookmark[] = [];
       }
-    }
-    this.owner.register('service:settings', SettingsStub);
-    await render(hbs`<Features::Quickstart::Newsfeed::Indicator/>`);
+      this.owner.register('service:newsfeed', NewsfeedStub);
+      await render(hbs`<Features::Quickstart::Newsfeed::Indicator/>`);
 
-    assert
-      .dom('[data-test-newsfeed-indicator')
-      .hasClass('newsfeed-indicator-position-right');
-  });
+      assert
+        .dom('[data-test-newsfeed-indicator]')
+        .hasStyle({ display: 'none' });
+    });
 
-  test("should have class 'newsfeed-indicator-position-left' when right-sided sidebar layout is selected", async function (this: Context, assert) {
-    class SettingsStub extends SettingsService {
-      get sidebarLayout() {
-        return SidebarLayout.rightBottom;
+    test('should be hidden when there are news, but sidebar is visible', async function (this: Context, assert) {
+      class NewsfeedStub extends NewsfeedService {
+        unreadBookmarks: Bookmark[] = [newsfeedMocks.unreadBookmark];
       }
-    }
-    this.owner.register('service:settings', SettingsStub);
-    await render(hbs`<Features::Quickstart::Newsfeed::Indicator/>`);
+      this.owner.register('service:newsfeed', NewsfeedStub);
+      class RendererStub extends RendererService {
+        leftSidebarExpanded = true;
+      }
+      this.owner.register('service:renderer', RendererStub);
+      await render(hbs`<Features::Quickstart::Newsfeed::Indicator/>`);
 
-    assert
-      .dom('[data-test-newsfeed-indicator')
-      .hasClass('newsfeed-indicator-position-left');
-  });
-});
+      assert
+        .dom('[data-test-newsfeed-indicator]')
+        .hasStyle({ display: 'none' });
+    });
+
+    test("should have class 'newsfeed-indicator-info' when there are unread bookmarks", async function (this: Context, assert) {
+      class NewsfeedStub extends NewsfeedService {
+        unreadBookmarks: Bookmark[] = [newsfeedMocks.unreadBookmark];
+      }
+      this.owner.register('service:newsfeed', NewsfeedStub);
+      await render(hbs`<Features::Quickstart::Newsfeed::Indicator/>`);
+
+      assert
+        .dom('[data-test-newsfeed-indicator')
+        .hasClass('newsfeed-indicator-info');
+    });
+
+    test("should have class 'newsfeed-indicator-position-right' when left-sided sidebar layout is selected", async function (this: Context, assert) {
+      class SettingsStub extends SettingsService {
+        get sidebarLayout() {
+          return SidebarLayout.leftTop;
+        }
+      }
+      this.owner.register('service:settings', SettingsStub);
+      await render(hbs`<Features::Quickstart::Newsfeed::Indicator/>`);
+
+      assert
+        .dom('[data-test-newsfeed-indicator')
+        .hasClass('newsfeed-indicator-position-right');
+    });
+
+    test("should have class 'newsfeed-indicator-position-left' when right-sided sidebar layout is selected", async function (this: Context, assert) {
+      class SettingsStub extends SettingsService {
+        get sidebarLayout() {
+          return SidebarLayout.rightBottom;
+        }
+      }
+      this.owner.register('service:settings', SettingsStub);
+      await render(hbs`<Features::Quickstart::Newsfeed::Indicator/>`);
+
+      assert
+        .dom('[data-test-newsfeed-indicator')
+        .hasClass('newsfeed-indicator-position-left');
+    });
+  }
+);
