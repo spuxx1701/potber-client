560a135b8aa7a67d8701cbc8f10863a2700d0970
Spuxx <ameisenfutter@outlook.com>
2023-03-10 21:23:19 +0100

    added accordion

---
+++ tests/integration/components/common/control/accordion-test.ts
@@ -0,0 +1,30 @@
+import { click, render, TestContext } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'potber-client/tests/helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+interface Context extends TestContext {
+  element: HTMLElement;
+}
+
+module(
+  'Integration | Component | Common | Control | Accordion',
+  function (hooks) {
+    setupRenderingTest(hooks);
+
+    test('properly hides and expands its content', async function (this: Context, assert) {
+      await render(
+        hbs`<Common::Control::Accordion>Hello World</Common::Control::Accordion>`
+      );
+
+      // Content is initially hidden
+      assert.dom('[data-test-accordion-content]').hasNoText();
+
+      // click the toggle
+      await click('[data-test-accordion-toggle]');
+
+      // Content is now visible
+      assert.dom('[data-test-accordion-content]').hasText('Hello World');
+    });
+  }
+);
