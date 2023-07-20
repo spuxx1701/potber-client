5a77db9394e3e4bb2272fb3670cc4d08d2807b54
Spuxx <ameisenfutter@outlook.com>
2023-07-20 16:23:33 +0200

    feat: mailbox routes are now slow routes

--- app/routes/authenticated/private-messages/inbound.ts
+++ app/routes/authenticated/private-messages/inbound.ts
@@ -1,9 +1,9 @@
-import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import { PrivateMessageFolder } from 'potber-client/models/private-message';
+import SlowRoute from 'potber-client/routes/slow';
 import CustomStore from 'potber-client/services/custom-store';
 
-export default class PrivateMessagesInboundRoute extends Route {
+export default class PrivateMessagesInboundRoute extends SlowRoute {
   @service declare store: CustomStore;
 
   async model() {
