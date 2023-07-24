1f02e6943623b6c770f7f86bb1fd374058604c3c
Spuxx <ameisenfutter@outlook.com>
2023-07-24 22:22:52 +0200

    feat: added menu button to private message view

--- app/components/nav/routes/private-messages/view/index.ts
+++ app/components/nav/routes/private-messages/view/index.ts
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import PrivateMessage from 'potber-client/models/private-message';
 import { createPrivateMessageSubtitle } from 'potber-client/utils/private-messages';
+import ENV from 'potber-client/config/environment';
 
 interface Signature {
   Args: {
@@ -11,5 +12,9 @@ interface Signature {
 export default class NavRoutesPrivateMessagesViewComponent extends Component<Signature> {
   get subtitle() {
     return createPrivateMessageSubtitle(this.args.message);
+  }
+
+  get originalUrl() {
+    return `${ENV.APP['FORUM_URL']}/pm/?a=2&mid=${this.args.message.id}`;
   }
 }
