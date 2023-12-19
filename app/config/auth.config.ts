f579dd777ea1933eaa43e42a569f461b246dfef7
spuxx1701 <hi@spuxx.dev>
2023-12-19 19:33:30 +0100

    fix: fixed wrong variable name

--- app/config/auth.config.ts
+++ app/config/auth.config.ts
@@ -2,7 +2,7 @@ import ENV from 'potber-client/config/environment';
 import { appConfig } from './app.config';
 
 export const authConfig = {
-  issuerUrl: ENV.APP['AUTH_SERVICE_URL'],
+  issuerUrl: ENV.APP['AUTH_ISSUER_URL'],
   clientId: '45a14ddc-e3d3-4b5b-a45a-a04946974adc',
   authorizeEndpoint: '/authorize',
   redirectUri: `${window.location.protocol}//${appConfig.hostname}/auth/callback`,
