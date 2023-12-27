6b1ba47e7746aca197049f78d68b0a7a804b107d
spuxx1701 <hi@spuxx.dev>
2023-12-27 13:07:08 +0100

    chore: Update app.config.ts with default values and added documentation

--- app/config/app.config.ts
+++ app/config/app.config.ts
@@ -1,15 +1,50 @@
 import ENV from 'potber-client/config/environment';
 
+/**
+ * Configuration object for the application.
+ */
 export const appConfig = {
+  /**
+   * The name of the application.
+   */
   name: 'potber',
-  hostname: ENV.APP['HOSTNAME'] as string,
-  apiUrl: ENV.APP['API_URL'] as string,
-  debug: ENV.APP['DEBUG'] as boolean,
-  memeHostUrl: ENV.APP['MEME_HOST_URL'] as string,
+  /**
+   * The hostname of the application.
+   */
+  hostname: (ENV.APP['HOSTNAME'] as string | undefined) ?? 'potber.de',
+  /**
+   * The API URL for the application.
+   */
+  apiUrl: (ENV.APP['API_URL'] as string | undefined) ?? 'https://api.potber.de',
+  /**
+   * Flag indicating whether debug mode is enabled.
+   */
+  debug: (ENV.APP['DEBUG'] as boolean | undefined) ?? false,
+  /**
+   * The URL for the meme host.
+   */
+  memeHostUrl:
+    (ENV.APP['MEME_HOST_URL'] as string | undefined) ?? 'https://potber.de',
+  /**
+   * The URL for the forum.
+   */
   forumUrl: 'https://forum.mods.de/',
+  /**
+   * The URL for the user page.
+   */
   userPageUrl: 'https://my.mods.de/',
+  /**
+   * The start date and time of the Christmas season.
+   */
   christmasSeasonStart: '12-01T00:00:00+01:00',
+  /**
+   * The end date and time of the Christmas season.
+   */
   christmasSeasonEnd: '12-31T23:59:59+01:00',
+  /**
+   * The threshold for HTTP timeout warning in milliseconds.
+   */
+  httpTimeoutWarningThreshold: 10000,
   /**
    * The default user group id. Other group ids define mods, admins and the like.
    */
