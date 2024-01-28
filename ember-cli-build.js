6e58be8aea14287403d192b730ffbb68cd129779
spuxx1701 <hi@spuxx.dev>
2024-01-28 18:57:52 +0100

    fix: Fixed config injection by disabling fingerprinting

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,8 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
+  const isProduction = process.env.EMBER_ENV === 'production';
+
   const app = new EmberApp(defaults, {
     babel: {
       sourceMaps: 'inline',
@@ -12,16 +14,16 @@ module.exports = function (defaults) {
       ],
     },
     sourcemaps: {
-      enabled: process.env.EMBER_ENV !== 'production',
+      enabled: !isProduction,
       extensions: ['js'],
     },
     fingerprint: {
-      enabled: process.env.EMBER_ENV === 'production',
-      exclude: ['images'],
+      enabled: false,
     },
   });
 
   const { Webpack } = require('@embroider/webpack');
+
   return require('@embroider/compat').compatBuild(app, Webpack, {
     packagerOptions: {
       // publicAssetURL is used similarly to Ember CLI's asset fingerprint prepend option.
@@ -29,7 +31,7 @@ module.exports = function (defaults) {
       // Embroider lets us send our own options to the style-loader
       cssLoaderOptions: {
         // don't create source maps in production
-        sourceMap: process.env.EMBER_ENV !== 'production',
+        sourceMap: !isProduction,
         // enable CSS modules
         modules: {
           // Enable local by default
@@ -38,28 +40,29 @@ module.exports = function (defaults) {
           localIdentName: '[local]__[sha512:hash:base64:5]',
         },
       },
-    },
-    webpackConfig: {
-      module: {
-        rules: [
-          {
-            // When webpack sees an import for a CSS files
-            test: /\.css$/i,
-            exclude: /node_modules/,
-            use: [
-              {
-                // use the PostCSS loader addon
-                loader: 'postcss-loader',
-                options: {
-                  sourceMap: process.env.EMBER_ENV !== 'production',
-                  postcssOptions: {
-                    config: './postcss.config.js',
+      webpackConfig: {
+        module: {
+          noParse: [/injected-config/i],
+          rules: [
+            {
+              // When webpack sees an import for a CSS files
+              test: /\.css$/i,
+              exclude: /node_modules/,
+              use: [
+                {
+                  // use the PostCSS loader addon
+                  loader: 'postcss-loader',
+                  options: {
+                    sourceMap: !isProduction,
+                    postcssOptions: {
+                      config: './postcss.config.js',
+                    },
                   },
                 },
-              },
-            ],
-          },
-        ],
+              ],
+            },
+          ],
+        },
       },
     },
   });
