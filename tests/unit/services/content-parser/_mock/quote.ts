d41aa6e163887329a317093bbe7254168674e850
spuxx1701 <accounts@spuxx.dev>
2024-08-18 02:08:57 +0200

    test: Fixed tests

--- tests/unit/services/content-parser/_mock/quote.ts
+++ tests/unit/services/content-parser/_mock/quote.ts
@@ -11,7 +11,7 @@ export const quoteTagMocks: ContentParserMock[] = [
     input: `[quote=1268185,1249841625,"Ameisenfutter"]<b>foo</b>[/quote]
 
     bar`,
-    expected: `<span class="quote"><a class="quote-header" href="https://test.potber.de/thread?TID=1268185&PID=1249841625"><p>Ameisenfutter</p></a><blockquote><b>foo</b></blockquote></span>
+    expected: `<span class="quote" data-author-name="Ameisenfutter"><a class="quote-header" href="https://test.potber.de/thread?TID=1268185&PID=1249841625"><p>Ameisenfutter</p></a><blockquote><b>foo</b></blockquote></span>
 
     bar`,
   },
@@ -20,35 +20,35 @@ export const quoteTagMocks: ContentParserMock[] = [
     [quote=219289,1249894747,"audax"]<b>
     Feature Wunsch: Pausieren von Videos wenn man weiter scrollt, bis sie nicht mehr als 30% sichtbar sind.
     </b>[/quote]
-    
+
     Puh. Mach mal issue auf github :D
     </b>[/quote]
-    
+
     Wird gemacht! Anschließend kannst du es ganz nach hinten ins Backlog schieben weil Viewports Krieg sind.`,
-    expected: `<span class="quote"><a class="quote-header" href="https://test.potber.de/thread?TID=219289&PID=1249894846"><p>Ameisenfutter</p></a><blockquote><b>
-    <span class="quote"><a class="quote-header" href="https://test.potber.de/thread?TID=219289&PID=1249894747"><p>audax</p></a><blockquote><b>
+    expected: `<span class="quote" data-author-name="Ameisenfutter"><a class="quote-header" href="https://test.potber.de/thread?TID=219289&PID=1249894846"><p>Ameisenfutter</p></a><blockquote><b>
+    <span class="quote" data-author-name="audax"><a class="quote-header" href="https://test.potber.de/thread?TID=219289&PID=1249894747"><p>audax</p></a><blockquote><b>
     Feature Wunsch: Pausieren von Videos wenn man weiter scrollt, bis sie nicht mehr als 30% sichtbar sind.
     </b></blockquote></span>
-    
+
     Puh. Mach mal issue auf github :D
     </b></blockquote></span>
-    
+
     Wird gemacht! Anschließend kannst du es ganz nach hinten ins Backlog schieben weil Viewports Krieg sind.`,
   },
   {
     input: `[quote=219289,1249882746,"[DtS]theSameButcher"]<b>
 
     Gibt es eigentlich auch schon Statistiken wieviele User, Posts via potber erstellt wurden?
-    
+
     </b>[/quote]
-    
+
     Ne, ich sammle solche Daten absichtlich nicht. Das könnte ich easy tun, aber:`,
-    expected: `<span class="quote"><a class="quote-header" href="https://test.potber.de/thread?TID=219289&PID=1249882746"><p>[DtS]theSameButcher</p></a><blockquote><b>
+    expected: `<span class="quote" data-author-name="[DtS]theSameButcher"><a class="quote-header" href="https://test.potber.de/thread?TID=219289&PID=1249882746"><p>[DtS]theSameButcher</p></a><blockquote><b>
 
     Gibt es eigentlich auch schon Statistiken wieviele User, Posts via potber erstellt wurden?
-    
+
     </b></blockquote></span>
-    
+
     Ne, ich sammle solche Daten absichtlich nicht. Das könnte ich easy tun, aber:`,
   },
 ];
